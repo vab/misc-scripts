c7fb65165cd29a7ae4adf3e3db0211626700795f
Andreas Scherer <andreas_github@freenet.de>
2016-09-17 17:11:31 +0200

    HTML5 header declaration.

--- party-table.pl
+++ party-table.pl
@@ -27,7 +27,8 @@ unless($ARGV[0])
 
 my @fps = `gpg --fingerprint --no-default-keyring --no-options --with-colons --keyring $ARGV[0] | egrep '^(pub|fpr):'`;
 
-print "<HTML><HEAD><TITLE>PGP Keysigning Party Keys</TITLE></HEAD>\n";
+print "<!DOCTYPE HTML>\n";
+print "<HTML LANG=\"en\"><HEAD><TITLE>PGP Keysigning Party Keys</TITLE></HEAD>\n";
 print "<BODY><TABLE BORDER=1>\n";
 print "<TR><TH>Key ID</TH><TH>Owner</TH><TH>Fingerprint</TH>";
 print "<TH>Size</TH><TH>Type</TH><TH>Key Info Matches?</TH><TH>Owner ID Matches?</TH></TR>\n";
